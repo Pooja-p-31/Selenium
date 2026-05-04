689180fae86581b52310ed1334561b986ea4e4cf
POOJA <poojap0703@gmail.com>
2026-05-04 07:57:21 +0530

    Added locator practice and navigation test cases

--- seleniumwebdriver/src/test/java/assignment/Assignment_03.java
+++ seleniumwebdriver/src/test/java/assignment/Assignment_03.java
@@ -16,7 +16,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Assignment_03 {x
+public class Assignment_03 {
 
     public static void main(String[] args) {
 
